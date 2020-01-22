b0dd2bf49a166e72dee76c3154e4ed1ae790f1ea
rahul2227 <rahul2227.l115@gmail.com>
2020-01-22 14:48:03 +0530

    connection with database

--- me/phpInput.php
+++ me/phpInput.php
@@ -1,16 +1,33 @@
 <?php
-echo 'welcome'.' '.$_POST['username'];
+include('second.php');
+// include_once('second.php');
+// require('second.php');
+// require_once('second.php');
+?>
 
+
+<?php
+if (isset($_POST['btn'])) {
+	echo 'Dear, '.' ' .$_POST['username'].' your details are :='.'<br>';
+echo $_POST['id'].'<br>';
+echo $_POST['Age'].'<br>';
+echo $_POST['Contact'].'<br>';
+echo $_POST['emailid'].'<br>';
+}
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>Input Form</title>
 </head>
 <body>
 	<form action="phpInput.php" method="post">
-	Name: <input type="text" name="username"><br>
+	ID: <input type="text" name="id" placeholder="Enter ID"><br>
+	Name: <input type="text" name="username" placeholder="Enter Name"><br>
+	Age: <input type="text" name="Age" placeholder="Enter Age"><br>
+	Contact: <input type="text" name="Contact" placeholder="Enter Contact"><br>
+	email: <input type="text" name="emailid" placeholder="Enter emailID"><br>
 	<input type="submit" name="btn">
 	</form>
 </body>
--- me/second.php
+++ me/second.php
@@ -5,7 +5,7 @@ $password = '';
 $databaseName = 'cseb2020';
 
 if(mysqli_connect($hostname, $username, $password, $databaseName)){
-	echo 'Connection successfull';
+	echo 'Connection successfull'.'<br>';
 } 
 else {
 	echo 'Connection failed!!' + mysqli_connect_error();
